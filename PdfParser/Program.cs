d472704e89be56b031132129a5a2b7acecf93129
Anurag1122-ctrl <53938013+Anurag1122-ctrl@users.noreply.github.com>
2019-10-01 12:58:47 +0530

    first update

---
+++ PdfParser/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+using Bytescout.PDFExtractor;
+using PdfParser.Db;
+using File = System.IO.File;
+
+namespace PdfParser
+{
+    internal class Program
+    {
+        private static readonly D11PdfDataEntities Db = new D11PdfDataEntities();
+        private static void Main()
+        {
+            var sw = default(StreamWriter);
+            try
+            {
+                var fileDir = new DirectoryInfo(ConfigurationManager.AppSettings["DirLocation"] + @"\Log\");
+                if (!fileDir.Exists)
+                    fileDir.Create();
+
+                sw = File.Exists(fileDir.FullName + "TrackLog.txt")
+                    ? File.AppendText(fileDir.FullName + "TrackLog.txt")
+                    : File.CreateText(fileDir.FullName + "TrackLog.txt");
+                sw.WriteLine("======================= Schedular Started, Dated : " + DateTime.Now + " ==========================");
+
+                ReadDownloadedFile(sw);
+            }
+            catch (Exception ex)
+            {
+                if (sw != null)
+                {
+                    sw.WriteLine("Main - " + DateTime.Now.ToString(CultureInfo.InvariantCulture));
+                    sw.WriteLine(ex.InnerException?.Message ?? ex.Message + " - " + DateTime.Now.ToString(CultureInfo.InvariantCulture));
+                }
+            }
+            finally
+            {
+                if (sw != null)
+                {
+                    sw.WriteLine("======================= SchedularEnded, Dated : " + DateTime.Now + " ==========================");
+                    sw.Close();
+                    sw.Dispose();
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Read downloaded files and insert records in database
+        /// </summary>
+        /// <param name="sw">Stream Writer</param>
+        /// <returns>Void</returns>
+        public static void ReadDownloadedFile(StreamWriter sw)
+        {
+            var path = new DirectoryInfo(ConfigurationManager.AppSettings["DirLocation"] + @"\teampdfs\");
+            var downloadDirectory = new DirectoryInfo(path.ToString());
+            if (downloadDirectory.Exists)
+            {
+                var files = downloadDirectory.GetFiles();
+
+                foreach (var file in files)
+                {
+                    try
+                    {
+                        var fInfo = new FileInfo(file.ToString());
+                        var newFile = ParsePdf(file, sw);
+                        if (!newFile)
+                        {
+                            sw.WriteLine("Error in File  Processed : " + fInfo.Name);
+                            Console.WriteLine("Error in File  Processed : " + fInfo.Name);
+
+                        }
+                        else
+                        {
+                            sw.WriteLine("File  Processed : " + fInfo.Name);
+                            Console.WriteLine("File  Processed : " + fInfo.Name);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        sw.WriteLine(" ReadDownloadedFile  - " + DateTime.Now.ToString(CultureInfo.InvariantCulture));
+                        sw.WriteLine(ex.Message + " - " + DateTime.Now.ToString(CultureInfo.InvariantCulture));
+                        Console.WriteLine(ex.Message + " - " + DateTime.Now.ToString(CultureInfo.InvariantCulture));
+                    }
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Parse Pdf and save into xlsx format
+        /// </summary>
+        /// <param name="file">File info for parse</param>
+        /// <param name="sw">stream writer </param>
+        /// <returns>File info of parsed file</returns>
+        public static bool ParsePdf(FileInfo file, StreamWriter sw)
+        {
+            try
+            {
+                var names = file.Name.Replace(".pdf", "").Split('-');
+                string teamName, value, matchName = names[0], matchId = names[1];
+                int columnCount, rowCount, teamNo = 0, count = 0;
+
+                var extractor = new StructuredExtractor
+                {
+                    RegistrationName = "demo",
+                    RegistrationKey = "demo"
+                };
+
+                // Load sample PDF document
+                extractor.LoadDocumentFromFile(file.FullName);
+
+                for (var pageIndex = 0; pageIndex < extractor.GetPageCount(); pageIndex++)
+                {
+                    Console.WriteLine("Starting extraction from page #" + pageIndex);
+                    extractor.PrepareStructure(pageIndex);
+                    rowCount = extractor.GetRowCount(pageIndex);
+
+                    for (var row = 0; row < rowCount; row++)
+                    {
+                        if (row < 2) continue;
+                        try
+                        {
+
+                            teamName = "";
+                            columnCount = extractor.GetColumnCount(pageIndex, row);
+
+                            for (var col = 0; col < columnCount; col++)
+                            {
+                                value = extractor.GetCellValue(pageIndex, row, col);
+                                if (string.IsNullOrEmpty(value))
+                                    continue;
+                                if (value.Contains("(") && value.Contains(")") && col == 0)
+                                {
+                                    teamName = value.Substring(0, value.LastIndexOf("("));
+                                    teamNo = value.Substring(value.IndexOf("(") + 1, value.Length - (value.IndexOf(")"))).ToInt();
+                                    break;
+                                }
+
+                                if (col != 0) continue;
+                                while (!value.Contains("(") && !value.Contains(")") && count < 5)
+                                {
+                                    value += $" {extractor.GetCellValue(pageIndex, ++row, col)}";
+                                    value = value.Replace("  ", " ");
+                                    count++;
+                                }
+                                count = 0;
+                                teamName = value.Substring(0, value.LastIndexOf("(")).Trim();
+                                teamNo = value.Substring(value.IndexOf("(") + 1, value.Length - (value.IndexOf(")"))).ToInt();
+                                break;
+                            }
+
+                            Db.MatchDatas.Add(new MatchData()
+                            {
+                                LeagueId = matchId.ToInt(),
+                                MatchName = matchName,
+                                TeamName = teamName,
+                                TeamNo = teamNo
+                            });
+                            Db.SaveChanges();
+                        }
+                        catch (Exception e)
+                        {
+                            sw.WriteLine("--------------------------------------------------------------------------------------");
+                            sw.WriteLine(extractor.GetCellValue(pageIndex, row, 0));
+                            sw.WriteLine("Error in file roe no :"+row+"--------" + file.Name);
+                            sw.WriteLine(e.GetBaseException().Message);
+                            sw.WriteLine("--------------------------------------------------------------------------------------");
+                        }
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetBaseException().Message);
+                sw.WriteLine(e.GetBaseException().Message);
+            }
+            return false;
+        }
+    }
+}
---
+++ PdfParser/StaticFunctions.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace PdfParser
+{
+    public static class StaticFunctions
+    {
+        public static string StrigHtml(this string input)
+        {
+            return Regex.Replace(input, "<.*?>", String.Empty);
+        }
+
+        public static int ToInt(this string value)
+        {
+            try
+            {
+                return Convert.ToInt32(value);
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+
+        public static double ToDouble(this string value)
+        {
+            try
+            {
+                return Convert.ToDouble(value);
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+
+        public static DateTime ToDateTime(this string value)
+        {
+            try
+            {
+                return Convert.ToDateTime(value);
+            }
+            catch (Exception)
+            {
+                return DateTime.Now;
+            }
+        }
+
+        public static void AddOrReplace(this IDictionary<string, object> dict, string key, object value)
+        {
+            if (dict.ContainsKey(key))
+                dict[key] = value;
+            else
+                dict.Add(key, value);
+        }
+
+        public static string ParseStringInTwoSteps(this string s, string start1, string stop1, string start2, string stop2)
+        {
+            string match;
+            int pos = Parse(s, 0, start1, stop1, out match);
+            if (pos > 0)
+                Parse(match, 0, start2, stop2, out match);
+            return match;
+        }
+
+        public static int Parse(string inText, int startIndex, string startString, string stopString, out string match)
+        {
+            return Parse(inText, startIndex, startString, stopString, out match, false);
+        }
+
+        public static int Parse(string inText, int startIndex, string startString, string stopString, out string match, bool caseSensitive)
+        {
+            match = "";
+            if (startIndex < 0 || string.IsNullOrEmpty(inText) || startIndex >= inText.Length)
+                return -1;
+            int num1 = !string.IsNullOrEmpty(startString) ? (!caseSensitive ? inText.IndexOfCaseInsensitive(startString, startIndex) : inText.IndexOf(startString, startIndex, StringComparison.Ordinal)) : startIndex;
+            if (num1 < 0)
+                return -1;
+            int startIndex1 = num1 + startString.Length;
+            for (int index = startIndex1; index < inText.Length; ++index)
+            {
+                startIndex1 = index;
+                switch (inText[index])
+                {
+                    case '\t':
+                    case '\n':
+                    case '\r':
+                    case ' ':
+                        continue;
+                    default:
+                        goto label_8;
+                }
+            }
+            label_8:
+            int num2 = !caseSensitive ? inText.IndexOfCaseInsensitive(stopString, startIndex1) : inText.IndexOf(stopString, startIndex1, StringComparison.Ordinal);
+            if (num2 < 0)
+            {
+                match = inText.SubstringLimit(startIndex1).Trim();
+                return startIndex1;
+            }
+            string str = inText.SubstringLimit(startIndex1, num2 - startIndex1).Trim();
+            if (str.Length <= 0)
+                return -1;
+            match = str;
+            return startIndex1;
+        }
+
+        public static string SubstringLimit(this string s, int startIndex)
+        {
+            return s.SubstringLimit(startIndex, s.Length);
+        }
+
+        public static string SubstringLimit(this string s, int startIndex, int maxLength)
+        {
+            if (string.IsNullOrEmpty(s) || startIndex < 0 || startIndex >= s.Length)
+                return "";
+            int length = Math.Min(s.Length - startIndex, maxLength);
+            if (length > 0)
+                return s.Substring(startIndex, length);
+            return "";
+        }
+
+        public static int IndexOfCaseInsensitive(this string s, string compare)
+        {
+            if (string.IsNullOrEmpty(s) || string.IsNullOrEmpty(compare))
+                return -1;
+            return s.ToUpper().IndexOf(compare.ToUpper(), StringComparison.Ordinal);
+        }
+
+        public static int IndexOfCaseInsensitive(this string s, string compare, int startIndex)
+        {
+            if (string.IsNullOrEmpty(s) || string.IsNullOrEmpty(compare))
+                return -1;
+            return s.ToUpper().IndexOf(compare.ToUpper(), startIndex, StringComparison.Ordinal);
+        }
+    }
+}
